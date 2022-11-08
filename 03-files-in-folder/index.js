ac870f2451f156e96ad5edf10b6fed6ddf75117e
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2022-11-08 12:44:40 +0300

    fix: delete sinc func in 3th task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const a = fs.readdirSync('03-files-in-folder/secret-folder', { withFileTypes: true });
-
-
-for (let i = 0; i < a.length; i++) {
-  if (a[i].isFile() == true) {
-    let name = a[i].name;
-    let bytes;
-    let est = path.extname(`03-files-in-folder/secret-folder/${name}`);
-    let name2 = name.replace(est, "");
-    fs.stat(`03-files-in-folder/secret-folder/${name}`, function (err, stats) {
-      if (err) { throw err; }
-      bytes = stats.size;
-      let el = `${name2} - ${est.slice(1)} - ${bytes} B`;
-      console.log(el);
-    })
+fs.readdir('03-files-in-folder/secret-folder', (err, files) => {
+  console.log(files)
+  if (err)
+    console.log(err);
+  else {
+    for (let i = 0; i < files.length; i++) {
+      fs.stat(`03-files-in-folder/secret-folder/${files[i]}`, function (err, stats) {
+        if (err) { throw err; }
+        if (stats.isFile() == true) {
+          let est = path.extname(`03-files-in-folder/secret-folder/${files[i]}`);
+          let name2 = files[i].replace(est, "");
+          let bytes = stats.size;
+          let el = `${name2} - ${est.slice(1)} - ${bytes} B`;
+          console.log(el);
+        }
+      })
+    }
   }
-}
+});
+
+
+
